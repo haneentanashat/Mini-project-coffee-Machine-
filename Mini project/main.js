23b0b8e5ad751b7b311492dae6e31a8b3e7ea252
haneentanashat <100359647+haneentanashat@users.noreply.github.com>
2022-04-10 01:08:45 +0300

    Update main.js

--- Mini project/main.js
+++ Mini project/main.js
@@ -5,30 +5,35 @@ let price = document.getElementById('payment');
 
 document.getElementById('buy').addEventListener("click", function () {
 
-    if(price.value==''){
-        document.getElementById('remian').innerHTML = 'Please Enter Money like 5 or 10 or 20 or 50 ';
-        document.getElementById('photo').style.display='none'
-        document.getElementById('remaining').style.display = 'none';
-    }
-    else if(document.getElementById('option1').checked == true) {
-            document.getElementById('photo').src = 'img/coffee.jpg';
-           
-        }
-        else if (document.getElementById('option2').checked == true) {
-            document.getElementById('photo').src = 'img/black.jpg';
-           
-        }
-        else if  (document.getElementById('option3').checked == true) {
-            document.getElementById('photo').src = 'img/milk111.jpg';
-            
-        }
-        
-        if (price.value > 1) {
+    if (document.getElementById('option1').checked == true) {
+        document.getElementById('photo').src = 'img/coffee.jpg';
 
-            let remain = price.value - 1;
-            document.getElementById('back').innerHTML = `${remain} JD`;
-        }
-        else{           
-             document.getElementById('back').innerHTML = "0 JD"; 
     }
-    })
+    else if (document.getElementById('option2').checked == true) {
+        document.getElementById('photo').src = 'img/black.jpg';
+
+    }
+    else if (document.getElementById('option3').checked == true) {
+        document.getElementById('photo').src = 'img/milk111.jpg';
+
+    }
+    let price = document.getElementById('payment');
+    let remain = price.value - 1;
+
+    switch (price.value) {
+        case "1": document.getElementById('back').innerHTML = "0 JD";
+            break;
+        case "5": document.getElementById('back').innerHTML = `${remain} JD`;
+            break;
+        case "10": document.getElementById('back').innerHTML = `${remain} JD`;
+            break;
+        case "20": document.getElementById('back').innerHTML = `${remain} JD`;
+            break;
+        case "50": document.getElementById('back').innerHTML = `${remain} JD`;
+            break;
+        default: document.getElementById('back').innerHTML = 'Please Enter Money like 1 or  5 or 10 or 20 or 50 ';
+            document.getElementById('photo').style.display = 'none';
+            document.getElementById('remaining').style.display = 'none';
+    }
+
+})
